61770f3fc42d49a91fbd0d5b70f104008d16cb13
MolochkovSA <wwwsergwww@mail.ru>
2024-05-12 18:53:34 +0300

    feat: add game-info

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,10 +1,31 @@
-import { GameSymbol } from './game-symbol'
+import clsx from 'clsx'
+import { Profile } from '../profile'
+import { CrossIcon } from './icons/cross-icon'
 
-export const GameInfo = ({ isDraw, winnerSymbol, currentStep }) => {
+export function GameInfo({ className }) {
   return (
-    <div className="mb-2.5">
-      {isDraw ? 'Ничья' : winnerSymbol ? 'Победили:' : 'Ход:'}
-      {!isDraw && <GameSymbol symbol={currentStep} />}
+    <div className={clsx(className, 'bg-white rounded-2xl shadow-md px-8 py-4 flex justify-between')}>
+      <div className="flex items-center gap-3">
+        <div className="relative">
+          <Profile className="w-[180px]" />
+          <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
+            <CrossIcon />
+          </div>
+        </div>
+        <div className=" h-6 w-px bg-slate-200"></div>
+        <div className="text-slate-900 text-lg leading-tight font-semibold">01:08</div>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <div className="text-orange-600 text-lg leading-tight font-semibold">00:08</div>
+        <div className=" h-6 w-px bg-slate-200"></div>
+        <div className="relative">
+          <Profile className="w-[180px]" />
+          <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
+            <CrossIcon />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
